a3418a03472ab8e857a44a95c96375eca4e82a7a
amillioner <anwar.akhror@gmail.com>
2019-10-18 13:03:27 -0400

    Homework for Day 9.
    Homework for Day 9.

---
+++ src/day10/HomeworkDay9.java
@@ -0,0 +1,9 @@
+public class HomeworkDay9{
+
+//1. Write a program that prompts the user to input a positive integer. It should then print the multiplication table of that number. 
+
+
+//2. Write a program to find the factorial value of any number entered through the keyboard. 
+//https://www.wikihow.com/Do-Factorials please follow this link for explanations.
+
+}
