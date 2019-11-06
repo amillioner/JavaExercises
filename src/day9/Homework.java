97bddc5ccfd75d8f5928d06bd86e9f007fdb4735
amillioner <anwar.akhror@gmail.com>
2019-11-06 08:28:07 -0500

    Update Homework.java

--- src/day9/Homework.java
+++ src/day9/Homework.java
@@ -2,94 +2,107 @@ package day9;
 
 public class Homework {
 
-    //TODO Task
-    // 1. Write program that gets two inputs from user then prints sum
-    // 2. Write simple calculator,
-    //     a. reads first input
-    //     b. reads operator (+, -, *, /)
-    //     c. reads second input
-    //     d. prints result
-    // 3. Write program that calculates from string:
-    //    ex: one plus nine => 10, four subtract 2 => 2
-//
-//    public class Test {
-//        public static void main(String[] args)
-//        {
-//            for (;;)
-//                System.out.println("TECHNO");
-//        }
+//class TECHNO throws ArithmeticException
+//{
+//    public static void main(String[]args) throws ArithmeticException
+//    {
+//        System.out.println(10/0);
+//        System.out.println("Hello TECHNO");
 //    }
+//}
+//
 ////Options:
-////1.TECHNO
-////2.Compile time error
-////3.Run time Exception
-////4.TECHNO (Infinitely)
-//
-//    class Test {
-//        public
-//        static void main(String[] args)
-//        {
-//            for (int i = 0; i < 3)
-//            System.out.println("TECHNO");
-//        }
-//    }
-////Options:
-////1.TECHNO TECHNO TECHNO
-////2.Compile time error
-////3.TECHNO (Infinitely)
-////4.No output
-//
-//    class Test {
-//        public static void main(String[] args)
-//        {
-//            boolean b = true;
-//            if (b = false) {
-//                System.out.println("HELLO");
-//            } else {
-//                System.out.println("BYE");
-//            }
-//        }
-//    }
-////Option:
-////1.HELLO
-////2.BYE
-////3.Compile time error: re- initialization
-////4.No output
-//
-//    public class Test {
-//        public
-//        static void main(String[] args)
-//        {
-//            int a = 10, b = 20;
-//            if (a < b) {
-//                if (a > b) {
-//                    System.out.println("HELLO TECHNO");
-//                } else {
-//                    System.out.println("WELCOME");
-//                }
-//            }
-//        }
-//    }
-////Option:
-////1.HELLO TECHNO
-////2.WELCOME
-////3.Compile time error
-////4.HELLO TECHNO WELCOME
-//
-//    class Test {
-//        public static void main(String[] args)
-//        {
-//            for (int i = 0;; i++) {
-//                System.out.println("HIII");
-//            }
-//            System.out.println("BYE");
-//        }
-//    }
-////Options:
-////1. HIII
-////2. HIII(infinitely)
-////3. BYE
+////1. Hello TECHNO
+////2. No Output
+////3. Run-time Exception
 ////4. Compile time error
+//
+//class TECHNO2 {
+//    public static void main(String[] args) throws TECHNO2 {
+//        Thread.sleep(10000);
+//        System.out.println("Hello TECHNO");
+//    }
+//}
+////Options:
+////1. Hello TECHNO
+////2. compile time error
+////3. No Output
+////4. Run-time exception
+//
+//class Test {
+//    void m1() throws ArithmeticException {
+//        throw new ArithmeticException("Calculation error");
+//    }
+//
+//    void m2() throws ArithmeticException {
+//        m1();
+//    }
+//
+//    void m3() {
+//        try {
+//            m2();
+//        } catch (ArithmeticException e) {
+//            System.out.println("ArithmeticException");
+//        }
+//    }
+//
+//    public static void main(String args[]) {
+//        Test t = new Test();
+//        t.m3();
+//        System.out.println("Handled by TECHNO");
+//    }
+//}
+////Options:
+////1. Calculation error
+////2. ArithmeticException
+////3. Handled by TECHNO
+////4. ArithmeticException Handled by TECHNO
+//
+//class Test2 {
+//    void Div() throws ArithmeticException {
+//        int a = 25, b = 0, rs;
+//        rs = a / b;
+//        System.out.print("\n\tThe result is : " + rs);
+//    }
+//
+//    public
+//    static void main(String[] args) {
+//        Test2 t = new Test2();
+//        try {
+//            t.Div();
+//        } catch (ArithmeticException e) {
+//            System.out.print("\n\tError : " + e.getMessage());
+//        }
+//        System.out.print("\n\tBYE TECHNO");
+//    }
+//}
+////Options:
+////1. BYE TECHNO
+////2. Compile time error
+////3. Error: / by zero
+////4. Error : / by zero
+////BYE TECHNO
+//
+//class Test3 {
+//    public static void main(String[] args) {
+//        fun();
+//    }
+//
+//    public static void fun() {
+//        moreFun();
+//    }
+//
+//    public static void moreFun() throws InterruptedException {
+//        Thread.sleep(10000);
+//        System.out.println("TECHNO");
+//    }
+//}
+////Options:
+////1. TECHNO
+////2. No Output
+////3. Compile time error
+////4. Run-time Exception
+
 
 
 }
