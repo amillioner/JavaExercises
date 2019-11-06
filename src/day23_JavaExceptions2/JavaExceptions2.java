d7fe3ff263d342191e4b69657f78adf6061263a7
amillioner <anwar.akhror@gmail.com>
2019-11-06 08:20:04 -0500

    Update JavaExceptions2.java

--- src/day23_JavaExceptions2/JavaExceptions2.java
+++ src/day23_JavaExceptions2/JavaExceptions2.java
@@ -2,4 +2,107 @@ package day23_JavaExceptions2;
 
 public class JavaExceptions2 {
 //    https://www.javatpoint.com/exception-handling-in-java
+  
+  //class Geeks throws ArithmeticException
+//{
+//    public static void main(String[]args) throws ArithmeticException
+//    {
+//        System.out.println(10/0);
+//        System.out.println("Hello Geeks");
+//    }
+//}
+//
+////Options:
+////1. Hello Geeks
+////2. No Output
+////3. Run-time Exception
+////4. Compile time error
+//
+//class Geeks2 {
+//    public static void main(String[] args) throws Geeks2 {
+//        Thread.sleep(10000);
+//        System.out.println("Hello Geeks");
+//    }
+//}
+////Options:
+////1. Hello Geeks
+////2. compile time error
+////3. No Output
+////4. Run-time exception
+//
+//class Test {
+//    void m1() throws ArithmeticException {
+//        throw new ArithmeticException("Calculation error");
+//    }
+//
+//    void m2() throws ArithmeticException {
+//        m1();
+//    }
+//
+//    void m3() {
+//        try {
+//            m2();
+//        } catch (ArithmeticException e) {
+//            System.out.println("ArithmeticException");
+//        }
+//    }
+//
+//    public static void main(String args[]) {
+//        Test t = new Test();
+//        t.m3();
+//        System.out.println("Handled by Geeks");
+//    }
+//}
+////Options:
+////1. Calculation error
+////2. ArithmeticException
+////3. Handled by Geeks
+////4. ArithmeticException Handled by Geeks
+//
+//class Test2 {
+//    void Div() throws ArithmeticException {
+//        int a = 25, b = 0, rs;
+//        rs = a / b;
+//        System.out.print("\n\tThe result is : " + rs);
+//    }
+//
+//    public
+//    static void main(String[] args) {
+//        Test2 t = new Test2();
+//        try {
+//            t.Div();
+//        } catch (ArithmeticException e) {
+//            System.out.print("\n\tError : " + e.getMessage());
+//        }
+//        System.out.print("\n\tBYE GEEKS");
+//    }
+//}
+////Options:
+////1. BYE GEEKS
+////2. Compile time error
+////3. Error: / by zero
+////4. Error : / by zero
+////BYE GEEKS
+//
+//class Test3 {
+//    public static void main(String[] args) {
+//        fun();
+//    }
+//
+//    public static void fun() {
+//        moreFun();
+//    }
+//
+//    public static void moreFun() throws InterruptedException {
+//        Thread.sleep(10000);
+//        System.out.println("GEEKS");
+//    }
+//}
+////Options:
+////1. GEEKS
+////2. No Output
+////3. Compile time error
+////4. Run-time Exception
+
+  
 }
